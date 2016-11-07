0762e95565ec8ab660294fd1920124f92ee21c6b
jeniatr <JeniaTr@yandex.ru>
2016-11-07 16:34:55 +0200

    modul connect  Db step 1

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/db/DBInterface.php';
+require_once __DIR__.'/db/CreateDB.php.php';
+require_once 'configuration.php';
+
+
+   $db = new CreateDB(SERVER,USER,PASS,DBNAME);
+
+
+/*
+    $names =  $db->select('*','names');
+    echo'<pre>';
+    print_r($names);
+    echo'</pre>';
+    //$db->insert('names', array('NULL','Влад'));
+    $db->closeConnection();
+*/
